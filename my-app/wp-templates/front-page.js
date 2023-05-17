2ddf05f8c08347581a5636864041d4b403162493
w3schools-test <aaronkaltman@gmail.com>
2023-05-17 15:33:09 -0400

    here goes nothing

---
+++ my-app/wp-templates/front-page.js
@@ -0,0 +1,87 @@
+import * as MENUS from 'constants/menus';
+
+import { useQuery, gql } from '@apollo/client';
+
+import AaronHero from '/components/AaronHero/AaronHero.js';
+import AaronServicesHome from '/components/AaronServicesHome/AaronServicesHome.js';
+import AaronStepper from '/components/AaronStepper/AaronStepper.js';
+import AaronFooter from '/components/AaronFooter/AaronFooter.js';
+
+import {
+    NavigationMenu,
+    SEO,
+    Posts,
+    Testimonials, AppBar,
+} from 'components';
+import { BlogInfoFragment } from 'fragments/GeneralSettings';
+
+const postsPerPage = 3;
+
+export default function Component() {
+    const { data, loading } = useQuery(Component.query, {
+        variables: Component.variables(),
+    });
+    if (loading) {
+        return null;
+    }
+
+    const { title: siteTitle, description: siteDescription } =
+        data?.generalSettings;
+
+
+    return (
+        <>
+            <SEO title={siteTitle} description={siteDescription} />
+
+            <AppBar />
+            <AaronHero />
+            <AaronServicesHome />
+            <AaronStepper />
+            <AaronFooter />
+        </>
+    );
+}
+
+Component.variables = () => {
+    return {
+        headerLocation: MENUS.PRIMARY_LOCATION,
+        footerLocation: MENUS.FOOTER_LOCATION,
+        first: postsPerPage,
+    };
+};
+
+Component.query = gql`
+  ${BlogInfoFragment}
+  ${NavigationMenu.fragments.entry}
+  ${Posts.fragments.entry}
+  ${Testimonials.fragments.entry}
+  query GetPageData(
+    $headerLocation: MenuLocationEnum
+    $footerLocation: MenuLocationEnum
+    $first: Int
+  ) {
+    posts(first: $first) {
+      nodes {
+        ...PostsItemFragment
+      }
+    }
+    testimonials {
+      nodes {
+        ...TestimonialsFragment
+      }
+    }
+    generalSettings {
+      ...BlogInfoFragment
+    }
+    headerMenuItems: menuItems(where: { location: $headerLocation }) {
+      nodes {
+        ...NavigationMenuItemFragment
+      }
+    }
+    footerMenuItems: menuItems(where: { location: $footerLocation }) {
+      nodes {
+        ...NavigationMenuItemFragment
+      }
+    }
+  }
+`;
